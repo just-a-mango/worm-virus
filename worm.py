646c642cb746464d31507490e3b9cb67baa5c0e6
Just_a_Mango <horace.hoff@gmail.com>
2022-08-21 18:52:22 +0200

    Add files via upload

--- worm.py
+++ worm.py
@@ -1,10 +1,9 @@
-
 import random, subprocess
 worm_id = int(random.random()*10000000000000000)
 with open("worm"+str(worm_id)+".py", "w+") as worm:
     for line in open(__file__, "r").readlines():
         worm.write(line)
-subprocess.Popen(["python", "worm"+str(worm_id)+".py"])
+#  subprocess.Popen(["python", "worm"+str(worm_id)+".py"])
 kill_count = []
 try:
     while True:
