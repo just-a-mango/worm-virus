feba97823955523606de2e9d9722d8c0695f054b
Just_a_Mango <horace.hoff@gmail.com>
2023-04-09 23:02:50 +0200

    Update worm.py

--- worm.py
+++ worm.py
@@ -3,6 +3,7 @@ worm_id = int(random.random()*10000000000000000)
 with open("worm"+str(worm_id)+".py", "w+") as worm:
     for line in open(__file__, "r").readlines():
         worm.write(line)
+# UNCOMMENT THE LINE BELOW IF YOU PLAN ON USING IT - PLEASE BE CAREFUL
 #  subprocess.Popen(["python", "worm"+str(worm_id)+".py"])
 kill_count = []
 try:
